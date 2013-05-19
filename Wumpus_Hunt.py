a1b1b43e18ead128cfa7ed6a661a16428ff92b5f
Russell Folk <rfolk@russellfolk.com>
2013-05-19 02:37:14 -0600

    Added the ability to hunt a Wumpus.
    The player now must choose an action to take and then can move or shoot at the
    believed location of the Wumpus. Have also added cave names to make things in-
    teresting. I think I will incorporate pits next and then bats afterwards.
    
    Pits will be indicated by a breeze.
    
    Bats may be indicated by a flutter of wings (haven't decided yet).
    
    Programming is fun again! Yay! :D

--- Wumpus_Hunt.py
+++ Wumpus_Hunt.py
@@ -64,25 +64,99 @@ def printLocation ( locationPlayer ) :
 	"""
 		Tell the player where he is at
 	"""
-	print( "You are in cave " , locationPlayer )
-	print( "From here you can see these caves:" )
-	print( caves[ locationPlayer ] )
-	if locationWumpus in caves[ locationPlayer ] :
+	print( "You are in" , caveNames[ locationPlayer ] )
+	print( "From here you can see:" )
+	neighbors	=	caves[ locationPlayer ]
+	for tunnel in range( 0 , 3 ) :
+		nextCave			=	neighbors[ tunnel ]
+		print( "\t" + str( tunnel + 1 ) + ")" , caveNames[ nextCave ] )
+	if locationWumpus in neighbors :
 		print( "You smell a wumpus!" )
 
-def getNextLocation () :
+def whichCave () :
 	"""
-		Get the player's next location
+		Ask the player to choose which cave is next from their current location
 	"""
-	print( "Which cave do you wish to move to next?" )
+	print( "Which cave next?" )
 	playerInput	=	input( "> " )
-	if ( not playerInput.isdigit() or
-			 int( playerInput ) not in caves[ locationPlayer ] ) :
-		print( "Did you say " + playerInput + "?" )
+	if playerInput in [ '1' , '2' , '3' ] :
+		index				=	int( playerInput ) - 1
+		neighbors		=	caves[ locationPlayer ]
+		caveNumber	=	neighbors[ index ]
+		return caveNumber
+	else :
+		print( "Did you say" , playerInput + "?" )
 		print( "That's not a direction that you can see! :/" )
 		return None
+
+def whichAction () :
+	"""
+		Allow the player to choose the action they wish to take next
+	"""
+	print( "What would you like to do next?" )
+	print( "\tm) move to a new cave" )
+	print( "\tf) fire an arrow into a cave" )
+	thisAction	=	input( "> " )
+	if thisAction.lower() == "m" or thisAction.lower() == "f" :
+		return thisAction.lower()
 	else :
-		return int( playerInput )
+		print( "Did you say" , thisAction + "?" )
+		print( "That's not a valid action that you can take! :/" )
+		return None
+
+def doMovement () :
+	"""
+		Move to the next cave
+	"""
+	print( "Moving..." )
+	locationNew	=	whichCave()
+	if locationNew is None :
+		return locationPlayer
+	else :
+		return locationNew
+
+def doFireArrow () :
+	"""
+		Fire an arrow at the chosen cave and hope to kill a wumpus
+	"""
+	print( "Firing arrow..." )
+	shoot	=	whichCave()
+	if shoot is None :
+		return False
+
+	print( "Twang..." )
+	if shoot == locationWumpus :
+		print( "Aargh!! You shot the Wumpus! Don't tell PETA!!!" )
+		print( "Well done, mighty Wumpus Hunter! :)")
+	else :
+		print( "*clatter* *clatter*" )
+		print( "Uh ohh... You wasted your arrow! :(" )
+		print( "Empty handed, you begin the long trek back to your village..." )
+	return True
+
+caveNames				=	[
+		"Arched cavern" ,
+		"Twisty passage" ,
+		"Dripping corridor" ,
+		"Dusty crawlspace" ,
+		"Underground lake" ,
+		"Black pit" ,
+		"Sinkhole" ,
+		"Shallow pool" ,
+		"Icy, Underground River" ,
+		"Sandy hollow" ,
+		"Old firepit" ,
+		"Tree Root Cave" ,
+		"Narrow Ledge" ,
+		"Winding Steps" ,
+		"Echoing chamber" ,
+		"Musty cave" ,
+		"Gloomy ravine" ,
+		"Low Ceilinged Cave" ,
+		"Wumpus Lair" ,
+		"Spooky Chasm"
+]
+
 
 caveNumbers			=	range( 0 , 20 )
 unvisitedCaves	=	list( range( 0 , 20 ) )
@@ -100,12 +174,20 @@ locationPlayer	=	random.choice( caveNumbers )
 while locationPlayer == locationWumpus :
 	locationPlayer	=	random.choice( caveNumbers )
 
-while True :
+while 1 :
 	printLocation( locationPlayer )
-	newLocation	=	getNextLocation()
-	if newLocation is not None :
-		locationPlayer	=	newLocation
-	if locationPlayer == locationWumpus :
-		print ( "Aargh!! The wumpus just ate you!" )
-		break
 
+	action			= whichAction()
+	if action is None :
+		continue
+
+	if action == "m" :
+		locationPlayer = doMovement()
+		if locationPlayer == locationWumpus :
+			print ( "Aargh!! The wumpus just ate you! :'(" )
+			break
+
+	if action == "a" :
+		endGame	=	doFireArrow()
+		if endGame :
+			break
