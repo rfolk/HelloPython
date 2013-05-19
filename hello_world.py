75e645b9f4fc039cfc98a47535f3e32c576cf039
Russell Folk <rfolk@russellfolk.com>
2013-05-19 01:32:06 -0600

    Simple Hello World program
    Time to see what Python is all about.

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#	print needs () now as it's a function
+#	print "Hello World!"
+print ( "Hello World!" )
+
+#	"raw_input ()" was replaced with "input ()"
+#	raw_input ( "hit enter to continue" )
+input ( "hit enter to continue" )
